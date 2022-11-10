7fc6b9370f40826093622cba68a6bc8769b802a8
hojoondo <hojoondo@daum.net>
2022-11-10 17:32:59 +0900

    Logo Activity

---
+++ src/app/src/main/java/com/example/user/plalarm/LogoActivity.java
@@ -0,0 +1,17 @@
+package com.example.user.plalarm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+public class LogoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logo);  //Logo Activity xml 파일 호출
+    }
+}
