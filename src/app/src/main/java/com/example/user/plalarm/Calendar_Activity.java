dafac3937550607acd0af195bddcd4f541153c81
sungwon-097 <csw2757@gmail.com>
2022-11-14 14:48:56 +0900

    #13 클래스 최적화
    익명클래스로 Week와 Calendar 내부에 decorate 직접 구현, 후에 리패키징하며 따로 관리 할 예정

--- src/app/src/main/java/com/example/user/plalarm/Calendar_Activity.java
+++ src/app/src/main/java/com/example/user/plalarm/Calendar_Activity.java
@@ -1,26 +1,23 @@
 package com.example.user.plalarm;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.style.ForegroundColorSpan;
 
 import com.prolificinteractive.materialcalendarview.CalendarDay;
-import com.prolificinteractive.materialcalendarview.CalendarMode;
+import com.prolificinteractive.materialcalendarview.DayViewDecorator;
+import com.prolificinteractive.materialcalendarview.DayViewFacade;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
-import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
 import com.prolificinteractive.materialcalendarview.format.TitleFormatter;
 
+import org.threeten.bp.DayOfWeek;
 import org.threeten.bp.LocalDate;
-
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
-import java.util.Locale;
 
 public class Calendar_Activity extends AppCompatActivity {
 
-    private MaterialCalendarView calendarView;
     final Calendar day = Calendar.getInstance();
 
     @Override
@@ -29,29 +26,40 @@ public class Calendar_Activity extends AppCompatActivity {
         setContentView(R.layout.activity_calendar);
 
 
-        calendarView = (MaterialCalendarView) findViewById(R.id.material_calendar);
+        MaterialCalendarView calendarView = (MaterialCalendarView) findViewById(R.id.material_calendar);
+
         calendarView.setSelectedDate(CalendarDay.today());
-
-        calendarView.addDecorators(new SundayDecorator(),new SaturdayDecorator());
-
+        calendarView.addDecorators(new DayViewDecorator() {
+            @Override
+            public boolean shouldDecorate(CalendarDay day) {
+                int weekDay = day.getDate().with(DayOfWeek.SATURDAY).getDayOfMonth();
+                return weekDay == day.getDay();
+            }
+            @Override
+            public void decorate(DayViewFacade view) {
+                view.addSpan(new ForegroundColorSpan(Color.BLUE));
+            }
+        }, new DayViewDecorator() {
+            @Override
+            public boolean shouldDecorate(CalendarDay day) {
+                int weekDay = day.getDate().with(DayOfWeek.SUNDAY).getDayOfMonth();
+                return weekDay == day.getDay();
+            }
+            @Override
+            public void decorate(DayViewFacade view) {
+                view.addSpan(new ForegroundColorSpan(Color.RED));
+            }
+        });
         calendarView.setTitleFormatter(new TitleFormatter() {
             @Override
             public CharSequence format(CalendarDay day) {
                 LocalDate inputText = day.getDate();
                 String[] calendarHeaderElements = inputText.toString().split("-");
-                StringBuilder calendarHeaderBuilder = new StringBuilder();
-                calendarHeaderBuilder.append(calendarHeaderElements[0])
-                        .append("년 ")
-                        .append(calendarHeaderElements[1])
-                        .append("월");
-                return calendarHeaderBuilder.toString();
+                return calendarHeaderElements[0] +
+                        "년 " +
+                        calendarHeaderElements[1] +
+                        "월";
             }
         });
-//        calendarView.setOnDateChangedListener(new OnDateSelectedListener() {
-//            @Override
-//            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
-//
-//            }
-//        });
     }
 }
--- src/app/src/main/java/com/example/user/plalarm/Week_Activity.java
+++ src/app/src/main/java/com/example/user/plalarm/Week_Activity.java
@@ -3,10 +3,14 @@ package com.example.user.plalarm;
 import androidx.annotation.IntegerRes;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.style.ForegroundColorSpan;
 
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.CalendarMode;
+import com.prolificinteractive.materialcalendarview.DayViewDecorator;
+import com.prolificinteractive.materialcalendarview.DayViewFacade;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.format.TitleFormatter;
 import com.prolificinteractive.materialcalendarview.format.WeekDayFormatter;
@@ -33,23 +37,40 @@ public class Week_Activity extends AppCompatActivity {
                 .setCalendarDisplayMode(CalendarMode.WEEKS)
                 .commit();
 
-        calendarView.addDecorators(new SaturdayDecorator(), new SundayDecorator());
+        calendarView.addDecorators(new DayViewDecorator() {
+            @Override
+            public boolean shouldDecorate(CalendarDay day) {
+                int weekDay = day.getDate().with(DayOfWeek.SATURDAY).getDayOfMonth();
+                return weekDay == day.getDay();
+            }
+            @Override
+            public void decorate(DayViewFacade view) {
+                view.addSpan(new ForegroundColorSpan(Color.BLUE));
+            }
+        }, new DayViewDecorator() {
+            @Override
+            public boolean shouldDecorate(CalendarDay day) {
+                int weekDay = day.getDate().with(DayOfWeek.SUNDAY).getDayOfMonth();
+                return weekDay == day.getDay();
+            }
+            @Override
+            public void decorate(DayViewFacade view) {
+                view.addSpan(new ForegroundColorSpan(Color.RED));
+            }
+        });
 
         calendarView.setTitleFormatter(new TitleFormatter() {//header title setting
             @Override
             public CharSequence format(CalendarDay day) {
                 LocalDate inputText = day.getDate();
                 String[] calendarHeaderElements = inputText.toString().split("-");
-                StringBuilder calendarHeaderBuilder = new StringBuilder();
-                calendarHeaderBuilder.append(calendarHeaderElements[0])
-                        .append("년 ")
-                        .append(calendarHeaderElements[1])
-                        .append("월");
-//                        .append(Integer.parseInt(calendarHeaderElements[2]) / 4 + 1)// 주계산하는 알고리즘 구현해봐야함
-//                        .append("주");
-                return calendarHeaderBuilder.toString();
+                //                        .append(Integer.parseInt(calendarHeaderElements[2]) / 4 + 1)// 주계산하는 알고리즘 구현해봐야함
+                //                        .append("주");
+                return calendarHeaderElements[0] +
+                        "년 " +
+                        calendarHeaderElements[1] +
+                        "월";
             }
         });
-
     }
 }
