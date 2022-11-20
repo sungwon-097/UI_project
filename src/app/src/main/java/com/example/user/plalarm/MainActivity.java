a171afeda5c677143cc67bc4e0dbf866ab98cae8
Hojoon Do <hojoondo@daum.net>
2022-11-20 20:47:31 +0900

    #17 Main Activity 기능 구현
    - Sound Button: 알림 ON/OFF
    - Setting Button: Setting Activity로 이동
    - New Button: 일정 생성 Activity로 이동(현재는 Main으로 이동하도록 설정, 이후 변경)
    
    - Fragment 부분은 아직 구현 X
    - Sound Button 상태 저장은 어떻게 할지 고민 필요

--- src/app/src/main/java/com/example/user/plalarm/MainActivity.java
+++ src/app/src/main/java/com/example/user/plalarm/MainActivity.java
@@ -2,13 +2,57 @@ package com.example.user.plalarm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button soundButton;
+    ImageButton settingButton;
+    ImageButton newButton;
+
+    ImageView notificationOnIcon;
+    ImageView notificationOffIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //3가지 Button 활성화
+        soundButton = (Button)findViewById(R.id.sound_button);
+        settingButton = (ImageButton)findViewById(R.id.setting_button);
+        newButton = (ImageButton)findViewById(R.id.new_button);
+
+        //soundButton 작동 시 표시 Image 전환 위한 View 선언
+        notificationOnIcon = (ImageView)findViewById(R.id.notification_on_icon);
+        notificationOffIcon = (ImageView)findViewById(R.id.notification_mute_icon);
+
+        soundButton.setOnClickListener(this);
+        settingButton.setOnClickListener(this);
+        newButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        //SoundButton을 클릭할 경우, 이미지를 교차 표시(visibility)
+        if (view == soundButton) {
+            notificationOnIcon.setVisibility(view.INVISIBLE);
+            notificationOffIcon.setVisibility(view.VISIBLE);
+        }
+        //SettingButton을 클릭할 경우, SettingActivity로 넘어감
+        else if (view == settingButton) {
+            Intent intent = new Intent(MainActivity.this, SettingActivity.class);
+            startActivity(intent);
+        }
+        //NewButton을 클릭할 경우, 일정 생성 Activity로 넘어감
+        //현재 일정 생성 Activity가 만들어지지 않았으므로 MainActivity로 가도록 해놓았음, 추후 수정
+        else if (view == newButton) {
+            Intent intent = new Intent(MainActivity.this, MainActivity.class);
+        }
     }
 }
