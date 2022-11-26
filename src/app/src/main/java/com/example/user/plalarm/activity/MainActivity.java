b0f90cec27e6aa249bda87cf35df80d3cb463e76
sungwon-097 <csw2757@gmail.com>
2022-11-27 00:55:45 +0900

    #39 리패키징
    불필요해진 파일 삭제 및 경로 이동

--- src/app/src/main/java/com/example/user/plalarm/activity/MainActivity.java
+++ src/app/src/main/java/com/example/user/plalarm/activity/MainActivity.java
@@ -1,19 +1,35 @@
 package com.example.user.plalarm;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.res.ResourcesCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TabHost;
 
+import com.example.user.plalarm.config.FirebaseConfig;
+import com.example.user.plalarm.fragment.CalendarFragment;
+import com.example.user.plalarm.fragment.DayFragment;
+import com.example.user.plalarm.fragment.WeekFragment;
+import com.example.user.plalarm.model.Event;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Calendar;
+import java.util.List;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button soundButton;
+    Button soundButton, notification, calendar, week, day;
     ImageButton settingButton;
     ImageButton newButton;
 
@@ -30,6 +46,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         settingButton = (ImageButton)findViewById(R.id.setting_button);
         newButton = (ImageButton)findViewById(R.id.new_button);
 
+        calendar = findViewById(R.id.calendar);
+        week = findViewById(R.id.week);
+        day = findViewById(R.id.day);
+        notification = findViewById(R.id.notification);
+
         //soundButton 작동 시 표시 Image 전환 위한 View 선언
         notificationOnIcon = (ImageView)findViewById(R.id.notification_on_icon);
         notificationOffIcon = (ImageView)findViewById(R.id.notification_mute_icon);
@@ -37,6 +58,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         soundButton.setOnClickListener(this);
         settingButton.setOnClickListener(this);
         newButton.setOnClickListener(this);
+
+        notification.setOnClickListener(this);
+        calendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentView(1);
+            }
+        });
+
+        week.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentView(2);
+            }
+        });
+
+        day.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentView(3);
+            }
+        });
 
 //        TabHost tabHost = findViewById(R.id.host);
 //        tabHost.setup();
@@ -57,6 +100,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //        tabHost.addTab(spec);
     }
 
+    private void fragmentView(int fragment){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        switch (fragment){
+            case 1:
+                CalendarFragment cf = new CalendarFragment();
+                transaction.replace(R.id.fragment_container, cf);
+                transaction.commit();
+                break;
+            case 2:
+                WeekFragment wf = new WeekFragment();
+                transaction.replace(R.id.fragment_container, wf);
+                transaction.commit();
+                break;
+            case 3:
+                DayFragment df = new DayFragment();
+                transaction.replace(R.id.fragment_container, df);
+                transaction.commit();
+                break;
+        }
+    }
     @Override
     public void onClick(View view) {
         //SoundButton을 클릭할 경우, 이미지를 교차 표시(visibility)
@@ -79,6 +143,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //현재 일정 생성 Activity가 만들어지지 않았으므로 MainActivity로 가도록 해놓았음, 추후 수정
         else if (view == newButton) {
             Intent intent = new Intent(MainActivity.this, EventActivity.class);
+            startActivity(intent);
+        }
+        else if(view == notification){
+            Intent intent = new Intent(MainActivity.this, NotificationActivity.class);
             startActivity(intent);
         }
     }
--- src/app/src/main/java/com/example/user/plalarm/activity/NotificationActivity.java
+++ src/app/src/main/java/com/example/user/plalarm/activity/NotificationActivity.java
@@ -9,14 +9,23 @@ import androidx.core.app.NotificationCompat;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.user.plalarm.config.FirebaseConfig;
 import com.example.user.plalarm.model.Event;
+import com.example.user.plalarm.service.TtsService;
+
+import java.util.List;
 
 public class NotificationActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -35,7 +44,10 @@ public class NotificationActivity extends AppCompatActivity implements View.OnCl
         String channelID = "DEFAULT";
 
         createNotificationChannel(channelID, "default", NotificationManager.IMPORTANCE_HIGH);
-        createNotification(channelID, 1, "title", "text");
+        createNotification(channelID, 1, "TTS 테스트", "테스트 중...");
+        TtsService ttsService = new TtsService(this);
+        ttsService.speak("tts 테스트"); // TODO : event.getContent() 로 치환
+        pendingIntent("com.example.user.plalarm");
     }
 
     public void createNotificationChannel(String channelID, String channelName, int importance){
@@ -46,15 +58,52 @@ public class NotificationActivity extends AppCompatActivity implements View.OnCl
     }
 
     public void createNotification(String channelID, int id, String title, String content){
+
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this, channelID)
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setSmallIcon(R.drawable.logo)
                 .setContentTitle(title)
                 .setContentText(content)
+//                .setSound()
                 .setDefaults(Notification.DEFAULT_SOUND | Notification.DEFAULT_VIBRATE);
 
         NotificationManager notificationManager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
         notificationManager.notify(id, builder.build());
+    }
+
+    public boolean getPackageList(String appName) {
+        boolean isExist = false;
+
+        PackageManager pkgMgr = getPackageManager();
+        List<ResolveInfo> mApps;
+        Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        mApps = pkgMgr.queryIntentActivities(mainIntent, 0);
+
+        try {
+            for (int i = 0; i < mApps.size(); i++) {
+                if(mApps.get(i).activityInfo.packageName.startsWith(appName)){
+                    isExist = true;
+                    break;
+                }
+            }
+        }
+        catch (Exception e) {
+            return false;
+        }
+        return isExist;
+    }
+
+    public void pendingIntent(String appName){
+        if(getPackageList(appName)) {
+            Intent intent = getPackageManager().getLaunchIntentForPackage(appName);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        }else{
+            String url = "market://details?id=" + appName;
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);
+        }
     }
 }
 
