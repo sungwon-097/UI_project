4afe4edc07e47c7515dadbba3173024715ca36b3
sungwon-097 <csw2757@gmail.com>
2022-11-27 01:04:30 +0900

    #10 Firebase 메소드 수정
    - putEventData 메소드에서 key의 값이 필드이름과 같지 않은 문제가 있었음.
    
    - getData에서 toObject 매소드를 통해 온전히 클래스형식으로 받으려 하였지만 deserialize문제에 직면. 방법을 고안해야함

--- src/app/src/main/java/com/example/user/plalarm/config/FirebaseConfig.java
+++ src/app/src/main/java/com/example/user/plalarm/config/FirebaseConfig.java
@@ -2,11 +2,9 @@ package com.example.user.plalarm.config;
 
 import static android.content.ContentValues.TAG;
 
-import android.annotation.SuppressLint;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
 
 import com.example.user.plalarm.model.Event;
 import com.example.user.plalarm.model.User;
@@ -18,6 +16,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -35,17 +34,19 @@ public class FirebaseConfig{
     // eventData 를 일정의 이름과 시작시간을 합친 문서로 저장(Ex event2022-11-08)
     public static void putEventData(@NonNull String path, Event event){
         Map<String, Object> eventEntity = new HashMap<>();
-        eventEntity.put("Title", event.getTitle());
-        eventEntity.put("Content", event.getContent());
-        eventEntity.put("StartTime", event.getStartTime());
-        eventEntity.put("EndTime", event.getEndTime());
-        eventEntity.put("IntentApp", event.getIntentApp());
+        eventEntity.put("title", event.getTitle());
+        eventEntity.put("content", event.getContent());
+        eventEntity.put("startTime", event.getStartTime());
+        eventEntity.put("endTime", event.getEndTime());
+        eventEntity.put("intentApp", event.getIntentApp());
         FirebaseFirestore.getInstance().collection(path).document(event.getTitle()+event.getStartTime()).set(eventEntity);
     }
 
     // collection 을 통해 서버에 저장된 정보를 가져옴
-    public static void getData(FirebaseFirestore db, @NonNull String collectionPath){
-        db.collection(collectionPath)
+    public static Map<String, Object> getData(@NonNull String collectionPath){
+
+        Map<String, Object> data = new HashMap<>();
+        FirebaseFirestore.getInstance().collection(collectionPath)
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
@@ -53,17 +54,20 @@ public class FirebaseConfig{
                         if (task.isSuccessful()) {
                             for (QueryDocumentSnapshot document : task.getResult()) {
                                 Log.d(TAG, document.getId() + " => " + document.getData());
+                                data.put("title", document.getData().get("title"));
+//                                event[0] = new Event((String) data.get("title"), (String) data.get("content"), (LocalDateTime)data.get("startTime"), (LocalDateTime) data.get("endTime"),(String) data.get("intentApp"));
                             }
                         } else {
                             Log.w(TAG, "Error getting documents.", task.getException());
                         }
                     }
                 });
+        return data;
     }
 
     // 컬렉션 내부의 문서 이름을 인자로 정보 삭제
-    public static void deleteData(FirebaseFirestore db, @NonNull String collectionPath, @NonNull String documentPath){
-        db.collection(collectionPath).document(documentPath)
+    public static void deleteData(@NonNull String collectionPath, @NonNull String documentPath){
+        FirebaseFirestore.getInstance().collection(collectionPath).document(documentPath)
                 .delete()
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
