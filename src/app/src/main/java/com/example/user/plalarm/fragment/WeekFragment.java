d20f7413d4841c50494b598cec57fb5bd909bc9e
JJamVa <hongi97@naver.com>
2022-11-28 17:18:39 +0900

    #48 Schdule Week 출력
    일시적으로 화면 출력 가능 상세구현 중

--- src/app/src/main/java/com/example/user/plalarm/fragment/WeekFragment.java
+++ src/app/src/main/java/com/example/user/plalarm/fragment/WeekFragment.java
@@ -12,6 +12,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.user.plalarm.R;
+import com.github.tlaabs.timetableview.TimetableView;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.CalendarMode;
 import com.prolificinteractive.materialcalendarview.DayViewDecorator;
@@ -22,62 +23,71 @@ import com.prolificinteractive.materialcalendarview.format.TitleFormatter;
 import org.threeten.bp.DayOfWeek;
 import org.threeten.bp.LocalDate;
 
-public class WeekFragment extends Fragment {
+public class WeekFragment extends Fragment implements View.OnClickListener{
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        TimetableView timetableView;
         View view = inflater.inflate(R.layout.fragment_week, container, false);
-        MaterialCalendarView calendarView = view.findViewById(R.id.material_week_calendar);
-        calendarView.setSelectedDate(CalendarDay.today());
+        timetableView = view.findViewById(R.id.timetable);
 
-        calendarView.state()
-                .edit()
-                .setCalendarDisplayMode(CalendarMode.WEEKS)
-                .commit();
-
-        calendarView.addDecorators(new DayViewDecorator() {
-            @Override
-            public boolean shouldDecorate(CalendarDay day) {
-                int weekDay = day.getDate().with(DayOfWeek.SATURDAY).getDayOfMonth();
-                return weekDay == day.getDay();
-            }
-
-            @Override
-            public void decorate(DayViewFacade view) {
-                view.addSpan(new ForegroundColorSpan(Color.BLUE));
-            }
-        }, new DayViewDecorator() {
-            @Override
-            public boolean shouldDecorate(CalendarDay day) {
-                int weekDay = day.getDate().with(DayOfWeek.SUNDAY).getDayOfMonth();
-                return weekDay == day.getDay();
-            }
-
-            @Override
-            public void decorate(DayViewFacade view) {
-                view.addSpan(new ForegroundColorSpan(Color.RED));
-            }
-        });
-
-        calendarView.setTitleFormatter(new TitleFormatter() {//header title setting
-            @Override
-            public CharSequence format(CalendarDay day) {
-                LocalDate inputText = day.getDate();
-                String[] calendarHeaderElements = inputText.toString().split("-");
-                //                        .append(Integer.parseInt(calendarHeaderElements[2]) / 4 + 1)// 주계산하는 알고리즘 구현해봐야함
-                //                        .append("주");
-                return calendarHeaderElements[0] +
-                        "년 " +
-                        calendarHeaderElements[1] +
-                        "월";
-            }
-        });
+//        MaterialCalendarView calendarView = view.findViewById(R.id.material_week_calendar);
+//        calendarView.setSelectedDate(CalendarDay.today());
+//
+//        calendarView.state()
+//                .edit()
+//                .setCalendarDisplayMode(CalendarMode.WEEKS)
+//                .commit();
+//
+//        calendarView.addDecorators(new DayViewDecorator() {
+//            @Override
+//            public boolean shouldDecorate(CalendarDay day) {
+//                int weekDay = day.getDate().with(DayOfWeek.SATURDAY).getDayOfMonth();
+//                return weekDay == day.getDay();
+//            }
+//
+//            @Override
+//            public void decorate(DayViewFacade view) {
+//                view.addSpan(new ForegroundColorSpan(Color.BLUE));
+//            }
+//        }, new DayViewDecorator() {
+//            @Override
+//            public boolean shouldDecorate(CalendarDay day) {
+//                int weekDay = day.getDate().with(DayOfWeek.SUNDAY).getDayOfMonth();
+//                return weekDay == day.getDay();
+//            }
+//
+//            @Override
+//            public void decorate(DayViewFacade view) {
+//                view.addSpan(new ForegroundColorSpan(Color.RED));
+//            }
+//        });
+//
+//        calendarView.setTitleFormatter(new TitleFormatter() {//header title setting
+//            @Override
+//            public CharSequence format(CalendarDay day) {
+//                LocalDate inputText = day.getDate();
+//                String[] calendarHeaderElements = inputText.toString().split("-");
+//                //                        .append(Integer.parseInt(calendarHeaderElements[2]) / 4 + 1)// 주계산하는 알고리즘 구현해봐야함
+//                //                        .append("주");
+//                return calendarHeaderElements[0] +
+//                        "년 " +
+//                        calendarHeaderElements[1] +
+//                        "월";
+//            }
+//        });
         return view;
     }
+
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
