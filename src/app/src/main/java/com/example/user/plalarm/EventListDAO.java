e16a342406012f8be2a2c839f56977c4f70cdc4e
sungwon-097 <csw2757@gmail.com>
2022-12-02 04:53:41 +0900

    #38 일주일간 또는 하루 일정 필터링

--- src/app/src/main/java/com/example/user/plalarm/EventListDAO.java
+++ src/app/src/main/java/com/example/user/plalarm/EventListDAO.java
@@ -1,17 +1,23 @@
 package com.example.user.plalarm;
 
+import static android.content.ContentValues.TAG;
+
+import android.util.Log;
+
 import com.example.user.plalarm.config.FirebaseDataContainer;
 import com.example.user.plalarm.model.Event;
 import com.example.user.plalarm.model.EventList;
 
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
 
 public class EventListDAO {
 
     String collectionPath;
     EventList eventList = new EventList();
+    LocalDate localDate = LocalDate.now();
+    String currentDate = String.valueOf(localDate).substring(0, 10);
 
     public EventListDAO(String collectionPath) {
         this.collectionPath = collectionPath;
@@ -20,8 +26,71 @@ public class EventListDAO {
     public EventList getEventItems(){
 
         FirebaseDataContainer firebaseDataContainer = new FirebaseDataContainer(collectionPath);
-//        eventList.setEventList(firebaseDataContainer.getContainer());
         eventList = firebaseDataContainer.getContainer();
         return eventList;
+    }
+
+    public EventList getDayEventItems(String date){
+
+        EventList returnList = new EventList();
+        FirebaseDataContainer firebaseDataContainer = new FirebaseDataContainer(collectionPath);
+        eventList = firebaseDataContainer.getContainer();
+        for (Event e:eventList.getEventList()){
+            if(e.getStartTime().substring(0, 10).equals(date)){
+                returnList.add(e);
+            }
+        }
+        return returnList;
+    }
+
+    public EventList getWeekEventItems(String date){
+
+        EventList returnList = new EventList();
+        FirebaseDataContainer firebaseDataContainer = new FirebaseDataContainer(collectionPath);
+        eventList = firebaseDataContainer.getContainer();
+        Log.d(TAG, "getWeekEventItems: "+ currentDate);
+        for (Event e:eventList.getEventList()){
+            if(compareIsCurrentWeek(e.getStartTime().substring(0, 10))){
+                Log.d(TAG, "getWeekEventItems: " + e.getStartTime().substring(0, 10));
+                returnList.add(e);
+            }
+            else
+                Log.d(TAG, "getWeekEventItems: False");
+        }
+        return returnList;
+    }
+
+    public long getWeekDays(String date){
+        String currentDate = date.substring(0, 10);
+        LocalDate localDate = LocalDate.parse(currentDate, DateTimeFormatter.ISO_DATE);
+        return localDate.getDayOfWeek().getValue();
+    }
+
+    public LocalDate getSundayDate(String date){
+        String currentDate = date.substring(0, 10);
+        LocalDate localDate = LocalDate.parse(currentDate, DateTimeFormatter.ISO_DATE);
+        localDate = localDate.minusDays(getWeekDays(date));
+
+        return localDate;
+    }
+
+    public LocalDate getNextSundayDate(String date){
+        String currentDate = date.substring(0, 10);
+        LocalDate localDate = LocalDate.parse(currentDate, DateTimeFormatter.ISO_DATE);
+        localDate = localDate.minusDays(getWeekDays(date));
+        localDate = localDate.plusDays(7);
+
+        return localDate;
+    }
+
+    public boolean compareIsCurrentWeek(String date){
+        LocalDate thisDate = LocalDate.parse(date, DateTimeFormatter.ISO_DATE);
+        Log.d(TAG, "compareIsCurrentWeek: " + getSundayDate(currentDate).compareTo(getNextSundayDate(currentDate)));
+        Period period = Period.between(getSundayDate(currentDate), getNextSundayDate(currentDate));
+        Log.d(TAG, "compareIsCurrentWeek: " + period.getDays());
+
+        if (getSundayDate(currentDate).isBefore(thisDate)||getSundayDate(currentDate).isEqual(thisDate))
+            return getNextSundayDate(currentDate).isAfter(thisDate);
+        return false;
     }
 }
