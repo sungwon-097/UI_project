eefb60c949fb8e5d9c34e86043b2cc89bb961a9b
Hojoon Do <hojoondo@daum.net>
2022-12-02 17:52:23 +0900

    #18 Logo Activity SharedPref
    SharedPref로 설정의 지정값 적용

--- src/app/src/main/java/com/example/user/plalarm/activity/LogoActivity.java
+++ src/app/src/main/java/com/example/user/plalarm/activity/LogoActivity.java
@@ -3,8 +3,10 @@ package com.example.user.plalarm.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.preference.PreferenceManager;
 
 import com.example.user.plalarm.R;
 import com.example.user.plalarm.config.FirebaseDataContainer;
@@ -16,15 +18,35 @@ public class LogoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logo);  //Logo Activity xml 파일 호출
         new FirebaseDataContainer("test");
-        new Handler().postDelayed(new Runnable()
-        {
-            @Override
-            public void run()
-            {
-                // Logo -> EventViewActivity
-                Intent intent = new Intent(LogoActivity.this, DayViewActivity.class);
-                startActivity(intent);
-            }
-        }, 1500);
+        // SharedPref 로 초기 화면 지정
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        int spinnerValue = sharedPref.getInt("calendarSpinner", 0);
+        if (spinnerValue == 0) {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(LogoActivity.this, EventViewActivity.class);
+                    startActivity(intent);
+                }
+            }, 1500);
+        }
+        else if (spinnerValue == 1) {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(LogoActivity.this, WeekViewActivity.class);
+                    startActivity(intent);
+                }
+            }, 1500);
+        }
+        else if (spinnerValue == 2) {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(LogoActivity.this, DayViewActivity.class);
+                    startActivity(intent);
+                }
+            }, 1500);
+        }
     }
 }
